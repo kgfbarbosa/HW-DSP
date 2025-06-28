70924d7166f5eaa1b5e851c121c221368daa5572
dkl5m <141453881+dkl5m@users.noreply.github.com>
2025-06-27 23:46:03 -0300

    add practice1

---
+++ DSP-2025.1/project1/peripherals_setup.c
@@ -0,0 +1,22 @@
+/*
+ * peripherals_setup.c
+ *
+ *  Created on: Jun 25, 2025
+ *      Author: klysm
+ */
+
+#include "peripherals_setup.h"
+
+void Setup_GPIO(void){
+
+
+
+
+
+}
+
+
+/*
+ * GPIO34 -> red
+ * GPIO31 -> blue
+*/
---
+++ DSP-2025.1/project1/peripherals_setup.h
@@ -0,0 +1,12 @@
+/*
+ * peripherals_setup.h
+ *
+ *  Created on: Jun 25, 2025
+ *      Author: klysm
+ */
+
+#infndef PERIPHERALS_SETUP_H_
+#include "F28x_Project.h"
+
+void Setup_GPIO(void);
+
