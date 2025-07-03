5f5562c0d9129d08d80432f302e6b4d72e499d5b
dkl5m <141453881+dkl5m@users.noreply.github.com>
2025-07-02 23:54:05 -0300

    add practice 3 DAC

---
+++ DSP-2025.1/practice3/project1/peripherals_setup.h
@@ -0,0 +1,15 @@
+/*
+ * peripherals_setup.h
+ *
+ *  Created on: Jun 25, 2025
+ *      Author: klysm
+ */
+
+#ifndef PERIPHERALS_SETUP_H_
+#define PERIPHERALS_SETUP_H_
+#include "F28x_Project.h"
+
+void Setup_GPIO(void);
+void Setup_DAC(void);
+
+#endif /* PERIPHERALS_SETUP_H_ */
