d5b2b9f4bd86676384b88c35f62a5da76ba6c9b1
dkl5m <141453881+dkl5m@users.noreply.github.com>
2025-07-08 23:36:57 -0300

    add practice 3 project 2

---
+++ DSP-2025.1/practice3/project2/main.c
@@ -0,0 +1,100 @@
+#include "peripherals_setup.h"
+#include "math.h"
+
+/**
+ * main.c *
+ *  Created on: Jun 25, 2025
+ *      Author: klysm
+ */
+
+uint32_t count = 0;                 // Initialize count variable
+
+interrupt void isr_timer0(void);    // Interrupt function
+
+uint16_t index = 0, sintable[400], costable[400];  // Initialize index, sine table and cosine table
+uint16_t offset1 = 0, offset2 = 0;  // Initialize offset 1 and 2
+
+int main(void)
+{
+    InitSysCtrl();          // Initialize System Resources
+
+    DINT;                   // Disable interrupts
+
+    InitPieCtrl();          // Initialize the PIE control registers to a
+                            //      known state.
+    IER = 0x0000;           // Disable CPU interrupts
+    IFR = 0x0000;           // Clear all CPU interrupt flags
+
+    InitPieVectTable();     // Initialize the PIE vector table
+
+
+    // Interrupts
+
+    EALLOW; //  Enables access to protected registers
+
+    PieVectTable.TIMER0_INT = &isr_timer0;
+    PieCtrlRegs.PIEIER1.bit.INTx7 = 1; // Enables INTx7
+
+    EDIS; //  Disables access to protected registers
+
+    IER |= M_INT1;          // Enables column 1
+
+
+    // Timer0 Configuration
+
+    InitCpuTimers();        // Initialize CPU Timers
+
+    ConfigCpuTimer(&CpuTimer0, 200, 50);
+
+    CpuTimer0Regs.TCR.all = 0x4000; // Enables Timer0 interrupt
+
+
+    // GPIO setup
+
+    Setup_GPIO();           // Custom function to configure GPIO pins as outputs
+
+    // DAC setup
+    Setup_DAC();
+
+    // Sin table setup
+    for (index = 0; index < 400; index++){
+        sintable[index] = (uint16_t)(1000.0*(1.0 + sin(6.28318531/400.*(float)index)));
+        costable[index] = (uint16_t)(1000.0*(1.0 + cos(6.28318531/400.*(float)index)));
+    }
+    index = 0;
+
+
+    EINT;                   // Enable global interrupt INTM
+    ERTM;                   // Enable global real-time debug interrupt DBGM
+
+    // Initialize both GPIO pins to low logic (0)
+    GpioDataRegs.GPBDAT.bit.GPIO34 = 0;
+    GpioDataRegs.GPADAT.bit.GPIO31 = 1;
+
+    while(1){
+        // Delay loop (software delay for timing)
+        for (count = 0; count < 0x005FFFFF; count++);
+
+        // Toggle GPIO34;
+        // If GPIO34 is 0, it becomes 1; if it's 1, it becomes 0
+        GpioDataRegs.GPBDAT.bit.GPIO34 ^= 1;
+
+        // Another delay loop, with different length to create de-synchronization
+        //  for (count = 0; count < 0x00AFFFFF; count++);
+
+        // Toggle GPIO31;
+        // If GPIO31 is 0, it becomes 1; if it's 1, it becomes 0
+      //  GpioDataRegs.GPADAT.bit.GPIO31 ^= 1;
+    }
+}
+
+interrupt void isr_timer0(void){
+    //GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;
+
+    index = (index == 399) ? 0 : (index + 1);
+
+    DacaRegs.DACVALS.all = offset1 + sintable[index]; // Sine table output
+    DacbRegs.DACVALS.all = offset2 + costable[index]; // Cosine table output
+
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+}
---
+++ DSP-2025.1/practice3/project2/peripherals_setup.c
@@ -0,0 +1,52 @@
+/*
+ * peripherals_setup.c
+ *
+ *  Created on: Jun 25, 2025
+ *      Author: klysm
+ */
+
+#include "peripherals_setup.h"
+
+void Setup_GPIO(void){
+    EALLOW; //  Enables access to protected registers
+
+    //GPIO34: output
+    GpioCtrlRegs.GPBGMUX1.bit.GPIO34 = 0;
+    GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
+    GpioCtrlRegs.GPBPUD.bit.GPIO34 = 1;                 // PUD on
+    GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;                 // Direction set as output
+                                                        // only valid for GPIO
+    GpioCtrlRegs.GPBCSEL1.bit.GPIO34 = GPIO_MUX_CPU1;   // Chooses CPU1
+
+    //GPIO31: output
+    GpioCtrlRegs.GPAGMUX2.bit.GPIO31 = 0;
+    GpioCtrlRegs.GPAMUX2.bit.GPIO31 = 0;
+    GpioCtrlRegs.GPAPUD.bit.GPIO31 = 1;                 // PUD on
+    GpioCtrlRegs.GPADIR.bit.GPIO31 = 1;                 // Direction set as output
+                                                        // only valid for GPIO
+    GpioCtrlRegs.GPACSEL4.bit.GPIO31 = GPIO_MUX_CPU1;   // Chooses CPU1
+
+    EDIS; //  Disables access to protected registers
+}
+
+void Setup_DAC(void){
+    EALLOW; //  Enables access to protected registers
+
+    // Configure DAC-A and DAC-B control registers
+    DacaRegs.DACCTL.all = 0x0001;
+    DacbRegs.DACCTL.all = 0x0001;
+
+    // Set DAC-A and DAC-B output to mid-range
+    DacaRegs.DACVALS.all = 0x0800;
+    DacbRegs.DACVALS.all = 0x0800;
+
+    // Enable DAC-A and DAC-B output
+    DacaRegs.DACOUTEN.bit.DACOUTEN = 1;                 // 0 = disable, 1 = enable
+    DacbRegs.DACOUTEN.bit.DACOUTEN = 1;                 // 0 = disable, 1 = enable
+
+    // DAC-A and DAC-B lock control register
+    DacaRegs.DACLOCK.all = 0x0000;
+    DacbRegs.DACLOCK.all = 0x0000;
+
+    EDIS; //  Disables access to protected registers
+}
